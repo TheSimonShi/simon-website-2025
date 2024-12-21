cbf6f853c0cb390d6370e4fffbb6b5fe86603f00
Simon <simonshi1997@gmail.com>
2024-12-20 21:25:26 -0800

    Socials page added v1

--- src/pages/socials.js
+++ src/pages/socials.js
@@ -1,17 +1,38 @@
 // Filename - "./pages/socials.js"
 import React from "react";
+import "./socials.css";
 
 const Socials = () => {
   return (
-    <div style={{ padding: "100px 20px", textAlign: "center" }}>
+    <div className="socials-container">
       <h1>Follow Me</h1>
       <p>Check out my social media platforms:</p>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        <li><a href="https://www.youtube.com/@thesimonshi" target="_blank" rel="noopener noreferrer">YouTube</a></li>
-        <li><a href="https://www.instagram.com/thesimonshi/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-        <li><a href="https://www.tiktok.com/@thesimonshi" target="_blank" rel="noopener noreferrer">TikTok</a></li>
-        <li><a href="https://twitter.com/TheSimonShi" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-      </ul>
+      <div className="social-links">
+        <a href="https://www.youtube.com/@simonshi" target="_blank" rel="noopener noreferrer" className="social-link youtube">
+          Triathlon YouTube
+        </a>
+        <a href="https://www.instagram.com/the_simonshi/" target="_blank" rel="noopener noreferrer" className="social-link instagram">
+          Instagram
+        </a>
+        <a href="https://www.tiktok.com/@thesimonshi" target="_blank" rel="noopener noreferrer" className="social-link tiktok">
+          TikTok
+        </a>
+        <a href="https://twitter.com/TheSimonShi" target="_blank" rel="noopener noreferrer" className="social-link twitter">
+          Twitter
+        </a>
+        <a href="https://www.strava.com/athletes/thesimonshi" target="_blank" rel="noopener noreferrer" className="social-link strava">
+          Strava
+        </a>
+        <a href="https://www.linkedin.com/in/simon-shi/" target="_blank" rel="noopener noreferrer" className="social-link linkedin">
+          LinkedIn
+        </a>
+        <a href="https://www.facebook.com/thesimonshi2" target="_blank" rel="noopener noreferrer" className="social-link facebook">
+          Facebook
+        </a>
+        <a href="https://www.youtube.com/@thesimonshi" target="_blank" rel="noopener noreferrer" className="social-link youtube">
+          Rubik's Cube YouTube
+        </a>
+      </div>
     </div>
   );
 };
