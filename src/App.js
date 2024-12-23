ba018d362bf786c7a615dee10b4092631b035f7e
Simon <simonshi1997@gmail.com>
2024-12-22 21:42:47 -0800

    Fixed error with home page indexing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; // Added Navigate
 import Navbar from "./components/NavBar";
-import ScrollToTop from "./components/ScrollToTop"; // Import the ScrollToTop component
+import ScrollToTop from "./components/ScrollToTop"; // Keeps pages at top on navigation
 import Footer from "./components/Footer";
-import Home from "./pages/home";
+import Home from "./pages/home"; // Home component added
 import Socials from "./pages/socials";
 import Coaching from "./pages/coaching";
 import Contact from "./pages/contact";
@@ -11,19 +11,18 @@ import RunCalculator from "./pages/runCalculator";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <ScrollToTop /> {/* Ensures each page starts at the top */}
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/home" />} /> {/* Redirect root to Home */}
+        <Route path="/home" element={<Home />} /> {/* Added Home */}
         <Route path="/socials" element={<Socials />} />
         <Route path="/coaching" element={<Coaching />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/runCalculator" element={<RunCalculator />} />
       </Routes>
-
       <Footer />
-      
     </Router>
   );
 }
